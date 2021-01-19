afd011a55eb5778de65c96172782986e4e97bcf5
amrashrv <a.shrv94@gmail.com>
2021-01-19 14:28:26 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://www.pngitem.com/pimgs/m/138-1385757_logo-random-png-download-logo-random-png-transparent.png" />
+    </header>;
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img src="https://rasterbatorapi.azureedge.net/images/r1.jpg" />
+    </div>
+    <div className="description">
+      <img src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"></img>
+    </div>
+    <div className="posts">
+      My posts
+      <div className="newpost">
+        New Post
+      </div>
+      <div className="item">
+        post 1
+      </div>
+      <div className="item">
+        post 2
+      </div>
+    </div>
+  </div>
+
+}
+export default Profile;
