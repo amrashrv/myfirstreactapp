9025fec76be4e7e3cbbdb0fc3e2bc6261167d1ce
amrashrv <a.shrv94@gmail.com>
2021-01-23 00:02:28 +0300

    changing css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import classes from './Header.module.css';
+import imageSrc from './pngegg.png'
 
 const Header = () => {
     return <header className={classes.header}>
-        <img src="https://www.pngitem.com/pimgs/m/138-1385757_logo-random-png-download-logo-random-png-transparent.png" />
+        <img title="logo" src={imageSrc} alt="logo"/>
     </header>;
 }
 export default Header;
